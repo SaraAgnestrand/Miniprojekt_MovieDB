ced5ba05b3bf6ca98356f2806588b7d8d24d4aef
Sara Agnestrand <sara@agnestrand.net>
2023-05-15 12:11:38 +0200

    NavLinks Header

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,17 +1,21 @@
-import MovieList from '../MovieList/MovieList';
-import MovieDetails from '../MovieDetails/MovieDetails';
-import About from '../About/About';
-import Contact from '../Contact/Contact';
+import MovieList from '../MovieList/MovieList'
+import MovieDetails from '../MovieDetails/MovieDetails'
+import About from '../About/About'
+import Contact from '../Contact/Contact'
+import { Routes, Route } from 'react-router-dom'
 
 
 function Main() {
   return (
-    <main>
-        <MovieList />
-        <MovieDetails />
-        <About />
-        <Contact />
-    </main>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<MovieList />} />
+        <Route path="/" element={<MovieDetails />} />
+        <Route path="/" element={<About />} />
+        <Route path="/" element={<Contact />} />
+      </Routes>
+    </div>
+    
+  );
 }
 export default Main;
--- src/components/NavLinks/NavLinks.jsx
+++ src/components/NavLinks/NavLinks.jsx
@@ -1,12 +1,41 @@
-import "./NavLinks.css";
+import "./NavLinks.css"
+import { NavLink } from "react-router-dom"
 
  function NavLinks() {
   return (
     <ul>
-      <li>Movies</li>
-      <li>About</li>
-      <li>Contact</li>
+      <li>
+        <NavLink style={({isActive}) => {
+                  return isActive ? { color: "orange" } : {} 
+                }}
+                to="../MyMovies"
+            >
+             Movies
+        </NavLink>
+      
+      </li>
+      <li>
+        <NavLink style={({isActive}) => {
+                  return isActive ? { color: "orange" } : {} 
+                }}
+                to="../About"
+            >
+             About
+        </NavLink>
+      
+      </li>
+      <li>
+        <NavLink style={({isActive}) => {
+                  return isActive ? { color: "orange" } : {} 
+                }}
+                to="../Contact"
+            >
+             Contact
+        </NavLink>
+      
+      </li>
     </ul>
+   
   );
 }
 
